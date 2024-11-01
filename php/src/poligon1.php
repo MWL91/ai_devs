5498ca4888d8c34ab6e3bf84508b0b323e091aad
Marcin Lenkowski <marcin.lenkowski@zyagnum.com>
2024-11-01 09:22:53 +0100

    php lifting

--- php/src/poligon1.php
+++ php/src/poligon1.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-$client = new GuzzleHttp\Client();
-$keysReq = $client->request('GET', 'https://poligon.aidevs.pl/dane.txt');
-$keys = array_filter(explode(PHP_EOL, $keysReq->getBody()));
+$getKeys = function(): array
+{
+    $client = new GuzzleHttp\Client();
+    $keysReq = $client->request('GET', 'https://poligon.aidevs.pl/dane.txt');
+    return array_filter(explode(PHP_EOL, $keysReq->getBody()));
+};
 
-$resultReq = verifyTask("POLIGON", $keys);
-
-dd($keys, json_decode($resultReq->getBody()));
+$resultReq = verifyTask("POLIGON", $getKeys());
+dd(json_decode($resultReq->getBody()));
